ca184ed0d0778807416d4ee4586ade6bc38916e2
unknown <renan.lifestyle@hotmail.com>
2015-07-30 00:36:29 -0300

    PhantomJS bind polyfill

--- capture/genBitmaps.js
+++ capture/genBitmaps.js
@@ -61,6 +61,48 @@ function capturePageSelectors(url,scenarios,viewports,bitmaps_reference,bitmaps_
 	    consoleBuffer = consoleBuffer + '\n' + message;
 	});
 
+	casper.on( 'page.initialized', function(){
+		console.log('AE!!');
+		this.evaluate(function(){
+			var isFunction = function(o) {
+				return typeof o == 'function';
+			};
+
+			var bind,
+				slice = [].slice,
+				proto = Function.prototype,
+				featureMap;
+
+			featureMap = {
+				'function-bind': 'bind'
+			};
+
+			function has(feature) {
+				var prop = featureMap[feature];
+				return isFunction(proto[prop]);
+			}
+
+			// check for missing features
+			if (!has('function-bind')) {
+				// adapted from Mozilla Developer Network example at
+				// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+				bind = function bind(obj) {
+					var args = slice.call(arguments, 1),
+						self = this,
+						nop = function() {
+						},
+						bound = function() {
+							return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+						};
+					nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
+					bound.prototype = new nop();
+					return bound;
+				};
+				proto.bind = bind;
+			}
+		});
+	});
+
 
 	casper.start();
 	// casper.viewport(1280,1024);
